8db9d8509e995e7ccfb71e829c6ff26b0ccee5ef
hemanbhullar <hemanbhullar8082@gmail.com>
2024-01-15 10:03:22 +0530

    Added login signup buttons to sidebar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,10 +1,20 @@
 import {AiFillCloseCircle} from 'react-icons/ai'
 import {FiMenu} from 'react-icons/fi'
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 
 import Footer from '../Components/Footer';
 
 function HomeLayout({ children }){
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    //for checking if user is logged in
+    const isLoggedIn = useSelector((state) => state ?.auth ?.isLoggedIn);
+
+    //for displaying the options acc to role
+    const role = useSelector((state) => state ?.auth ?.role);
 
     function changeWidth() {
         const drawerSide = document.getElementsByClassName("drawer-side");
@@ -16,7 +26,16 @@ function HomeLayout({ children }){
         element[0].checked = false;
         
         const drawerSide = document.getElementsByClassName("drawer-side");
-        drawerSide[0].style.width = 0;
+        drawerSide[0].style.width = '0';
+    }
+
+    function handleLogout(e){
+        e.preventDefault();
+
+        // const res = await dispatch(logout());
+
+        // if(res ?.payload ?.success)
+        navigate("/");
     }
 
     return (
@@ -42,7 +61,11 @@ function HomeLayout({ children }){
                         <li>
                             <Link to="/">Home</Link>
                         </li>
-
+                        {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to="/admin/dashboard">Admin DashBoard</Link>
+                            </li>
+                        )}
                         <li>
                             <Link to="/courses">All Courses</Link>
                         </li>
@@ -53,6 +76,31 @@ function HomeLayout({ children }){
                         <li>
                             <Link to="/about">About Us</Link>
                         </li>
+
+                        {!isLoggedIn && (
+                            <li className='absolute bottom-4 w-[90%]'>
+                            <div className="w-full flex items-center justify-center">
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link to="/login">Login</Link>
+                                 </button>
+                                <button className="bg-pink-500 hover:bg-pink-700 text-white  px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link to="/signup">Signup</Link>
+                                 </button>
+                            </div>
+                            </li>
+                        )}
+                        {isLoggedIn && (
+                            <li className='absolute bottom-4 w-[90%]'>
+                            <div className="w-full flex items-center justify-center">
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link to="/user/profile">Profile</Link>
+                                 </button>
+                                <button className="bg-pink-500 hover:bg-pink-700 text-white  px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link onClick={handleLogout}>Logout</Link>
+                                 </button>
+                            </div>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'primary': '#3490dc', // Change this to your desired primary color
+        'secondary': '#f6993f', // Change this to your desired secondary color
+      },
+    },
   },
   plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
 }
