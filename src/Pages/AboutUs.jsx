3c7de5a7e757b8ac67f16b9941a90dd980609a72
hemanbhullar <hemanbhullar8082@gmail.com>
2024-01-15 16:18:02 +0530

    Cleaned up our about us

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,12 +1,12 @@
 import aboutMainImage from "../Assets/images/aboutMainImage.png"
-import apj from "../Assets/Images/apj.png";
-import billGates from "../Assets/Images/billGates.png";
-import einstein from "../Assets/Images/einstein.png";
-import nelsonMandela from "../Assets/Images/nelsonMandela.png";
-import steveJobs from "../Assets/Images/steveJobs.png";
+import CarouselSlide from "../Components/CarouselSlide";
+import { celebrities } from "../Constants/CelebrityData";
 import HomeLayout from "../Layouts/HomeLayout";
 
 function AboutUs() {
+
+    
+
     return (
         <HomeLayout>
             <div className="pl-20 pt-20 flex flex-col text-white">
@@ -28,71 +28,11 @@ function AboutUs() {
                 </div>
 
                 <div className="carousel w-1/2 my-16 m-auto">
-                    <div id="slide1" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 ps-[15%]">
-                            <img src={nelsonMandela} className="w-40 rounded-full border-2 border-gray-400" />
-                            <p className="text-xl text-gray-200">
-                                {"Education is the most powerful tool you can use to change the world."}
-                            </p>
-                            <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide5" className="btn btn-circle">❮</a>
-                                <a href="#slide2" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="slide2" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 ps-[15%]">
-                            <img src={apj} className="w-40 rounded-full border-2 border-gray-400" />
-                            <p className="text-xl text-gray-200">
-                                {"To succeed in your mission, you must have single-minded devotion to your goal."}
-                            </p>
-                            <h3 className="text-2xl font-semibold">APJ Abdul Kalam</h3>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide1" className="btn btn-circle">❮</a>
-                                <a href="#slide3" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="slide3" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 ps-[15%]">
-                            <img src={einstein} className="w-40 rounded-full border-2 border-gray-400" />
-                            <p className="text-xl text-gray-200">
-                                {"If you want your children to be intelligent, read them fairy tales. If you want them to be more intelligent, read them more fairy tales."}
-                            </p>
-                            <h3 className="text-2xl font-semibold">Albert Einstein</h3>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide2" className="btn btn-circle">❮</a>
-                                <a href="#slide4" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="slide4" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 ps-[15%]">
-                            <img src={steveJobs} className="w-40 rounded-full border-2 border-gray-400" />
-                            <p className="text-xl text-gray-200">
-                                {"Innovation distinguishes between a leader and a follower."}
-                            </p>
-                            <h3 className="text-2xl font-semibold">Steve Jobs</h3>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide3" className="btn btn-circle">❮</a>
-                                <a href="#slide5" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="slide5" className="carousel-item relative w-full">
-                        <div className="flex flex-col items-center justify-center gap-4 ps-[15%]">
-                            <img src={billGates} className="w-40 rounded-full border-2 border-gray-400" />
-                            <p className="text-xl text-gray-200">
-                                {"Patience is a key element of success."}
-                            </p>
-                            <h3 className="text-2xl font-semibold">Bill Gates</h3>
-                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href="#slide4" className="btn btn-circle">❮</a>
-                                <a href="#slide1" className="btn btn-circle">❯</a>
-                            </div>
-                        </div>
-                    </div>
+                    {celebrities && celebrities.map(celebrity => <CarouselSlide 
+                    {...celebrity} 
+                    key={celebrity.slideNumber}
+                     totalSlides={celebrities.length}
+                     />)}
                 </div>
             </div>
         </HomeLayout>
