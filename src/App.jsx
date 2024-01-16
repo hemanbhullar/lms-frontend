80248202ade38d0135cac5ebd4c3d47e0d27ff67
hemanbhullar <hemanbhullar8082@gmail.com>
2024-01-16 15:47:20 +0530

    Added requireAuth

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css'
 
 import { Route, Routes } from 'react-router-dom'
 
+import RequireAuth from './Components/Auth/RequireAuth';
 import AboutUs from './Pages/AboutUs';
 import Contact from './Pages/Contact';
 import CourseDescription from './Pages/Course/CourseDescription';
 import CourseList from './Pages/Course/CourseList';
+import CreateCourse from './Pages/Course/CreateCourse';
 import Denied from './Pages/Denied';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
@@ -28,8 +30,13 @@ function App() {
 
       <Route path="/signup" element={<Signup/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/login" element={<Login/>}/>
 
-      <Route path="*" element={<NotFound/>}></Route>
+      <Route element={<RequireAuth allowedRoles={["ADMIN"]}/>}>
+         <Route path="/course/create" element={<CreateCourse />} />
+      </Route>
+
+      <Route path='*' element={<NotFound />}></Route>
     </Routes>
     </>
   )
